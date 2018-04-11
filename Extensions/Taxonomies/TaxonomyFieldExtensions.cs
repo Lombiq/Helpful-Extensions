154374c46839728e1605f994b1ff276e99e9faf0
Benedek Farkas <benedek.farkas@lombiq.com>
2018-04-11 15:43:44 +0200

    Adding GetFirstTerm() to TaxonomyFieldExtensions
    --HG--
    branch : orchard-upgrade-1.11

--- Extensions/Taxonomies/TaxonomyFieldExtensions.cs
+++ Extensions/Taxonomies/TaxonomyFieldExtensions.cs
@@ -6,8 +6,11 @@ namespace Piedone.HelpfulExtensions.Taxonomies
 {
     public static class TaxonomyFieldExtensions
     {
+        public static TermPart GetFirstTerm(this TaxonomyField field) =>
+            field?.Terms?.Any() ?? false ? TermPart.Sort(field.Terms).FirstOrDefault() : null;
+
         public static string GetFirstTermName(this TaxonomyField field) =>
-            field?.Terms?.Any() ?? false ? TermPart.Sort(field.Terms).First().Name : "";
+            GetFirstTerm(field)?.Name;
 
         public static string GetTermNames(this TaxonomyField field) =>
             field?.Terms?.Any() ?? false ? string.Join(", ", TermPart.Sort(field.Terms).Select(term => term.Name)) : "";
