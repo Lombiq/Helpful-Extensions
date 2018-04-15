986ebdb079c2a5394e7b59fea57bec8f9a1b3f7c
Benedek Farkas <benedek.farkas@lombiq.com>
2018-04-16 00:15:24 +0200

    Adding RequestTokens and IdentityResolverTokens
    --HG--
    branch : orchard-upgrade-1.11

---
+++ Extensions/Tokens/IdentityResolverTokens.cs
@@ -0,0 +1,62 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using Orchard.Localization;
+using Orchard.Tokens;
+using System;
+
+namespace Piedone.HelpfulExtensions.Contents.Tokens
+{
+    [OrchardFeature(Constants.FeatureNames.Tokens)]
+    public class IdentityResolverTokens : ITokenProvider
+    {
+        private readonly IContentManager _contentManager;
+
+        public Localizer T { get; set; }
+
+
+        public IdentityResolverTokens(IContentManager contentManager)
+        {
+            _contentManager = contentManager;
+
+            T = NullLocalizer.Instance;
+        }
+
+
+        public void Describe(DescribeContext context)
+        {
+            context.For("IdentityResolver", T("Identity resolution"), T("Identity tokens to get the content item."))
+                .Token("Identity:*", T("Identity:*<content identity>"), T("The content identity value to acccess."))
+                .Token("Content", T("Content Items"), T("Content Items"));
+        }
+
+        public void Evaluate(EvaluateContext context)
+        {
+            context.For("IdentityResolver", () => _contentManager)
+                .Token(token => token.StartsWith("Identity:", StringComparison.OrdinalIgnoreCase) ? token.Substring("Identity:".Length) : null, GetContentItem)
+                .Chain(FilterChainParam, "Content", GetContentItem);
+        }
+
+
+        private static Tuple<string, string> FilterChainParam(string token)
+        {
+            var tokenLength = "Identity:".Length;
+            var equalsIndex = token.IndexOf('=');
+            var dotIndex = token.IndexOf('.', equalsIndex);
+            var chainIndex = token.IndexOf('.');
+
+            return token.StartsWith("Identity:", StringComparison.OrdinalIgnoreCase) && chainIndex > tokenLength ?
+                new Tuple<string, string>(token.Substring(tokenLength, dotIndex - tokenLength), token.Substring(dotIndex + 1, token.Length - dotIndex - 1)) :
+                null;
+        }
+
+        private static ContentItem GetContentItem(string contentIdentity, IContentManager contentManager)
+        {
+            var startIndex = contentIdentity.IndexOf('=');
+            var index = contentIdentity.IndexOf(('.'), startIndex);
+
+            if (index > -1) contentIdentity = contentIdentity.Substring(0, index);
+
+            return contentManager.ResolveIdentity(new ContentIdentity(contentIdentity));
+        }
+    }
+}
---
+++ Extensions/Tokens/RequestTokens.cs
@@ -0,0 +1,43 @@
+﻿using Orchard;
+using Orchard.Environment.Extensions;
+using Orchard.Localization;
+using Orchard.Tokens;
+
+namespace Piedone.HelpfulExtensions.Contents.Tokens
+{
+    [OrchardFeature(Constants.FeatureNames.Tokens)]
+    // Extends the original RequestTokens found in the Orchard.Tokens module.
+    public class RequestTokens : ITokenProvider
+    {
+        private readonly IWorkContextAccessor _wca;
+
+        public Localizer T { get; set; }
+
+
+        public RequestTokens(IWorkContextAccessor wca)
+        {
+            _wca = wca;
+
+            T = NullLocalizer.Instance;
+        }
+
+
+        public void Describe(DescribeContext context)
+        {
+            context
+                .For("Request", T("Http Request"), T("Current Http Request tokens."))
+                .Token("FullQueryString", T("FullQueryString"), T("The full Query String in the current HTTP request."));
+        }
+
+        public void Evaluate(EvaluateContext context)
+        {
+            var httpContext = _wca.GetContext().HttpContext;
+            if (httpContext == null) return;
+
+            context
+                .For("Request", httpContext.Request)
+                .Token("FullQueryString", request => request.Url.Query);
+        }
+    }
+
+}
