ccca4241cbbb4d324daefdc55849e30fd4a93b0e
Benedek Farkas <benedek.farkas@lombiq.com>
2018-11-08 16:35:39 +0100

    Adding ContentManagerExtensions.GetContentItemByIdentity
    --HG--
    branch : orchard-upgrade-1.11

--- Extensions/ContentManagerExtensions.cs
+++ Extensions/ContentManagerExtensions.cs
@@ -4,5 +4,8 @@
     {
         public static IContent NewDummyItem(this IContentManager contentManager) =>
             contentManager.New("Dummy");
+
+        public static ContentItem GetContentItemByIdentity(this IContentManager contentManager, string identity) =>
+            contentManager.ResolveIdentity(new ContentIdentity(identity));
     }
 }
