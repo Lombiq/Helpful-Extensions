ec92c0d83c3253ec0cfd030faf4beda530c29bf1
Benedek Farkas <benedek.farkas@lombiq.com>
2019-03-24 19:48:58 +0100

    Adding JsonConverter.TryDeserialize extension method
    --HG--
    branch : orchard-upgrade-1.11

---
+++ Extensions/JsonConverterExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Orchard.Services;
+
+namespace Orchard.Services
+{
+    public static class JsonConverterExtensions
+    {
+        public static bool TryDeserialize<T>(this IJsonConverter jsonConverter, string jsonString, out T result)
+        {
+            result = default(T);
+
+            try
+            {
+                result = jsonConverter.Deserialize<T>(jsonString);
+            }
+            catch (JsonException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
