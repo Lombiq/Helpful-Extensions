cfef21a1a1eb95ae97b773fb68745d2c5cea82cd
Benedek Farkas <benedek.farkas@lombiq.com>
2019-03-24 20:25:25 +0100

    Adding Request.QueryStringValuesJson token
    --HG--
    branch : orchard-upgrade-1.11

--- Extensions/Tokens/RequestTokens.cs
+++ Extensions/Tokens/RequestTokens.cs
@@ -1,7 +1,10 @@
 ﻿using Orchard;
 using Orchard.Environment.Extensions;
 using Orchard.Localization;
+using Orchard.Services;
 using Orchard.Tokens;
+using System;
+using System.Linq;
 
 namespace Piedone.HelpfulExtensions.Contents.Tokens
 {
@@ -10,13 +13,15 @@ namespace Piedone.HelpfulExtensions.Contents.Tokens
     public class RequestTokens : ITokenProvider
     {
         private readonly IWorkContextAccessor _wca;
+        private readonly IJsonConverter _jsonConverter;
 
         public Localizer T { get; set; }
 
 
-        public RequestTokens(IWorkContextAccessor wca)
+        public RequestTokens(IWorkContextAccessor wca, IJsonConverter jsonConverter)
         {
             _wca = wca;
+            _jsonConverter = jsonConverter;
 
             T = NullLocalizer.Instance;
         }
@@ -26,7 +31,8 @@ namespace Piedone.HelpfulExtensions.Contents.Tokens
         {
             context
                 .For("Request", T("Http Request"), T("Current Http Request tokens."))
-                .Token("FullQueryString", T("FullQueryString"), T("The full Query String in the current HTTP request."));
+                .Token("FullQueryString", T("FullQueryString"), T("The full Query String in the current HTTP request."))
+                .Token("QueryStringValuesJson:*", T("QueryStringValuesJson:<element>"), T("The value(s) of the specified Query String parameter key serialized to JSON."));
         }
 
         public void Evaluate(EvaluateContext context)
@@ -36,7 +42,16 @@ namespace Piedone.HelpfulExtensions.Contents.Tokens
 
             context
                 .For("Request", httpContext.Request)
-                .Token("FullQueryString", request => request.Url.Query);
+                .Token("FullQueryString", request => request.Url.Query)
+                .Token(
+                    token => token.StartsWith("QueryStringValuesJson:", StringComparison.OrdinalIgnoreCase) ? token.Substring("QueryStringValuesJson:".Length) : null,
+                    (token, request) =>
+                    {
+                        var values = request.QueryString.GetValues(token);
+
+                        return values?.Any() ?? false ? _jsonConverter.Serialize(values) : null;
+                    }
+                );
         }
     }
 
