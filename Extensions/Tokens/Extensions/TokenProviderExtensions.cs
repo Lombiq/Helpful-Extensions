81bd53636954cc88b3d34bc266becb848ee1f7f1
Benedek Farkas <benedek.farkas@lombiq.com>
2019-06-07 17:28:50 +0200

    Renaming EvaluateForExtensions to TokenProviderExtensions and adding DescribeFor.ContentPartToken
    --HG--
    branch : orchard-upgrade-1.11

---
+++ Extensions/Tokens/Extensions/TokenProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Localization;
+using Piedone.HelpfulLibraries.Contents;
+using System;
+
+namespace Orchard.Tokens
+{
+    public static class TokenProviderExtensions
+    {
+        public static DescribeFor ContentPartToken<TPart>(
+            this DescribeFor describeFor,
+            string tokenName,
+            LocalizedString name,
+            LocalizedString description) where TPart : ContentPart =>
+            describeFor.Token($"{typeof(TPart).Name}.{tokenName}", name, description);
+
+        public static EvaluateFor<IContent> ContentPartToken<TPart>(
+            this EvaluateFor<IContent> evaluateFor,
+            string tokenName,
+            Func<TPart, object> tokenValue) where TPart : ContentPart =>
+            evaluateFor.Token($"{typeof(TPart).Name}.{tokenName}", content => tokenValue(content.AsOrThrow<TPart>()));
+    }
+}
