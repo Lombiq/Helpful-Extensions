032b3966032aa3c27bfb2a77b06ddcbb506b7d39
Márk Bartha <mark.bartha@lombiq.com>
2019-12-16 17:52:07 +0100

    Adding DateTimeExtensions and StringExtensions
    --HG--
    branch : orchard-upgrade-1.11

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Orchard.Localization.Services;
+using Orchard.Services;
+using System.Globalization;
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToIsoDateTimeString(this DateTime dateTime) => dateTime.ToString("O");
+
+        public static string ToIsoDateString(this DateTime dateTime) => dateTime.ToString("yyyy-MM-dd");
+
+        public static DateTime FirstDayOfMonth(this DateTime dateTime) =>
+            new DateTime(dateTime.Year, dateTime.Month, 1, 0, 0, 0, dateTime.Kind);
+
+        public static DateTime LastDayOfMonth(this DateTime dateTime) =>
+            FirstDayOfMonth(dateTime).AddMonths(1).AddDays(-1);
+
+        /// <summary>
+        /// Converts the given date to USA format.
+        /// </summary>
+        /// <param name="dateTime">Date to be converted to USA format.</param>
+        /// <returns>USA-formatted date string.</returns>
+        public static string ConvertToUsaDateFormat(this DateTime dateTime) =>
+            dateTime.ToString("d", CultureInfo.CreateSpecificCulture("en-US"));
+
+        /// <summary>
+        /// Converts the given date to USA format.
+        /// </summary>
+        /// <param name="dateTime">Date to be converted to USA format.</param>
+        /// <returns>USA-formatted date string, empty string if null.</returns>
+        public static string ConvertToUsaDateFormat(this DateTime? dateTime) =>
+            dateTime?.ConvertToUsaDateFormat() ?? "";
+
+        /// <summary>
+        /// Converts the given date to long USA format.
+        /// </summary>
+        /// <param name="dateTime">Date to be converted to USA format, months with letters.</param>
+        /// <returns>USA-formatted long date string.</returns>
+        public static string ConvertToLongUsaDateFormat(this DateTime dateTime) =>
+            dateTime.ToString("D", CultureInfo.CreateSpecificCulture("en-US"));
+
+        /// <summary>
+        /// Converts the given date to long USA format.
+        /// </summary>
+        /// <param name="dateTime">Date to be converted to USA format, months with letters.</param>
+        /// <returns>USA-formatted long date string, empty string if null.</returns>
+        public static string ConvertToLongUsaDateFormat(this DateTime? dateTime) =>
+            dateTime?.ConvertToLongUsaDateFormat() ?? "";
+
+        /// <summary>
+        /// Converts the given date-time to USA format.
+        /// </summary>
+        /// <param name="dateTime">Date-time to be converted to USA format.</param>
+        /// <returns>USA-formatted date-time string.</returns>
+        public static string ConvertToUsaDateTimeFormat(this DateTime dateTime) =>
+            dateTime.ToString("g", CultureInfo.CreateSpecificCulture("en-US"));
+
+        /// <summary>
+        /// Converts the given date-time to USA format.
+        /// </summary>
+        /// <param name="dateTime">Date-time to be converted to USA format.</param>
+        /// <returns>USA-formatted date-time string, empty string if null.</returns>
+        public static string ConvertToUsaDateTimeFormat(this DateTime? dateTime) =>
+            dateTime?.ConvertToUsaDateTimeFormat() ?? "";
+
+        // This name is oddly specific...
+        public static string DateTimeOrUtcNowToSiteTimeZoneInUsaDateTimeFormat(
+            this DateTime? dateTime,
+            IClock clock,
+            IDateLocalizationServices dateLocalizationServices) =>
+            dateLocalizationServices.ConvertToSiteTimeZone(dateTime ?? clock.UtcNow).ConvertToUsaDateTimeFormat();
+
+        public static string DateTimeOrUtcNowToSiteTimeZoneInUsaDateFormat(
+            this DateTime? dateTime,
+            IClock clock,
+            IDateLocalizationServices dateLocalizationServices) =>
+            dateLocalizationServices.ConvertToSiteTimeZone(dateTime ?? clock.UtcNow).ConvertToUsaDateFormat();
+
+        public static string ConvertToUsaTimeFormat(this DateTime? time) =>
+            time?.ToString("hh:mm tt") ?? "";
+
+        public static string DateTimeOrUtcNowToSiteTimeZoneInUsaTimeFormat(
+            this DateTime? dateTime,
+            IClock clock,
+            IDateLocalizationServices dateLocalizationServices) =>
+            (dateLocalizationServices.ConvertToSiteTimeZone(dateTime ?? clock.UtcNow) as DateTime?).ConvertToUsaTimeFormat();
+    }
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,85 @@
+﻿using Orchard.Services;
+using Orchard.Utility.Extensions;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Splits string by any new line characters found the given text.
+        /// </summary>
+        /// <param name="text">Text that needs to be split.</param>
+        /// <param name="stringSplitOptions">String split options to be used.</param>
+        /// <returns>Text split by new line characters.</returns>
+        public static string[] SplitByNewLine(this string text, StringSplitOptions stringSplitOptions = StringSplitOptions.None) =>
+            text.ReplaceNewLinesWith(Environment.NewLine).Split(new[] { Environment.NewLine }, stringSplitOptions);
+
+        /// <summary>
+        /// Replaces invalid file name characters to a replacement character.
+        /// </summary>
+        /// <param name="unsafeFileName">File name that needs to be converted to a valid file name.</param>
+        /// <param name="replacementCharacter">Character to be used when replacing invalid characters.</param>
+        /// <returns>Valid file name.</returns>
+        public static string ToSafeFileName(this string unsafeFileName, char replacementCharacter = '_')
+        {
+            var invalidCharacters = Path.GetInvalidFileNameChars();
+            if (invalidCharacters.Contains(replacementCharacter))
+                throw new ArgumentException("Replacement character should be a valid file name character.", nameof(replacementCharacter));
+
+            var fileName = unsafeFileName;
+            foreach (var invalidCharacter in invalidCharacters)
+            {
+                fileName = fileName.Replace(invalidCharacter, '_');
+            }
+
+            return fileName;
+        }
+
+        public static string RemoveTags(this string htmlText, params string[] tags)
+        {
+            if (string.IsNullOrEmpty(htmlText) || !tags.Any()) return htmlText;
+
+            foreach (var tag in tags)
+                htmlText = Regex.Replace(htmlText, $@"<{tag}[\d\D]*?>[\d\D]*?</{tag}>", "", RegexOptions.Singleline);
+
+            return htmlText;
+        }
+
+        /// <summary>
+        /// Removes HTML tags from the given text.
+        /// </summary>
+        /// <param name="htmlText">Text containing HTML tags.</param>
+        /// <returns>Text without HTML tags.</returns>
+        public static string StripHtml(this string htmlText) =>
+            string.IsNullOrEmpty(htmlText) ? htmlText : Regex.Replace(htmlText, "<.*?>", "", RegexOptions.Singleline);
+
+        /// <summary>
+        /// Similar to HtmlClassify, this function converts a string to a CSS class name,
+        /// but the result is compliant with BEM standards.
+        /// </summary>
+        /// <param name="text">The string to convert to a BEM class name.</param>
+        /// <returns>The BEM-ified CSS class name.</returns>
+        public static string HtmlBemClassify(this string text)
+        {
+            var segments = text.HtmlClassify().Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!segments.Any()) return null;
+
+            if (!segments.Skip(1).Any()) return segments[0];
+
+            for (int i = 1; i < segments.Count(); i++)
+                segments[i] = segments[i].Skip(1).Any() ?
+                    char.ToUpper(segments[i][0]) + segments[i].Substring(1) : segments[i].ToUpper();
+
+            return string.Join("", segments);
+        }
+
+        public static string ConvertFromJsonStringArrayToCommaSeparatedString(this string serialized, IJsonConverter jsonConverter) =>
+            jsonConverter.TryDeserialize<IEnumerable<string>>(serialized, out var values) ?
+                string.Join(", ", values.OrderBy(x => x)) : "";
+    }
+}
