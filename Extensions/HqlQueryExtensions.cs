b59145874d853e38998b08a08a650d81a297d31a
Benedek Farkas <benedek.farkas@lombiq.com>
2020-03-04 21:29:07 +0100

    Adding HqlQueryExtensions (with NullQuery function)
    --HG--
    branch : orchard-upgrade-1.11

---
+++ Extensions/HqlQueryExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Orchard.ContentManagement
+{
+    public static class HqlQueryExtensions
+    {
+        /// <summary>
+        /// Causes the query not to return any results.
+        /// </summary>
+        public static IHqlQuery NullQuery(this IHqlQuery query) =>
+            query.Where(alias => alias.ContentItem(), filter => filter.Eq("Id", 0));
+    }
+}
