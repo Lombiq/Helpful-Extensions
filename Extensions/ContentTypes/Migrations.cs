9f990262677e584f9df812271e9a77398c524274
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-04-26 23:23:58 +0200

    Adding part positions to Page

--- Extensions/ContentTypes/Migrations.cs
+++ Extensions/ContentTypes/Migrations.cs
@@ -1,4 +1,4 @@
-﻿using OrchardCore.Autoroute.Models;
+using OrchardCore.Autoroute.Models;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
 using OrchardCore.Data.Migration;
@@ -25,18 +25,30 @@ namespace Lombiq.HelpfulExtensions.Extensions.ContentTypes
                 .Draftable()
                 .Listable()
                 .Versionable()
-                .WithPart("TitlePart")
-                .WithPart("FlowPart")
+                .WithPart("TitlePart", part => part.WithPosition("0"))
                 .WithPart("AutoroutePart", part => part
+                    .WithPosition("1")
                     .WithSettings(new AutoroutePartSettings
                     {
                         ShowHomepageOption = true,
                         AllowCustomPath = true
                     })
                 )
+                .WithPart("FlowPart", part => part.WithPosition("2"))
             );
 
-            return 1;
+            return 2;
+        }
+
+        public int UpdateFrom1()
+        {
+            _contentDefinitionManager.AlterTypeDefinition(Page, builder => builder
+                .WithPart("TitlePart", part => part.WithPosition("0"))
+                .WithPart("AutoroutePart", part => part.WithPosition("1"))
+                .WithPart("FlowPart", part => part.WithPosition("2"))
+            );
+
+            return 2;
         }
     }
 }
