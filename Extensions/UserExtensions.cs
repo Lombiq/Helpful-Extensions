e3a0a564c5595f8bd01e73b5f0b460f77c8e7799
Benedek Farkas <benedek.farkas@lombiq.com>
2020-04-28 00:05:03 +0200

    Adding UserExtensions (and function to check if a user is the owner of a content item)

---
+++ Extensions/UserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Aspects;
+
+namespace Orchard.Security
+{
+    public static class UserExtensions
+    {
+        public static bool IsOwnerOf(this IUser user, IContent content) =>
+            user == null || content == null ? false : user.Id == content.As<ICommonPart>()?.Owner?.Id;
+    }
+}
