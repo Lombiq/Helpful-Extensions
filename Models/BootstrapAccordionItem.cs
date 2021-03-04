bfff40898ca2f0acaa294eaa10e2af79f618b7bb
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-04 19:13:38 +0100

    Create Bootstrap Accordion shape.

---
+++ Models/BootstrapAccordionItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using System.Collections.Generic;
+
+namespace Lombiq.HelpfulExtensions.Models
+{
+    public class BootstrapAccordionItem
+    {
+        public LocalizedHtmlString Title { get; set; }
+        public dynamic Shape { get; set; }
+    }
+}
