37bd1eb41265eefa536ce5f49f24946a64ea6b29
Dávid El-Saig <david.el-saig@lombiq.com>
2021-05-07 18:55:18 +0200

    Docs.

--- Services/ITestingMarkerService.cs
+++ Services/ITestingMarkerService.cs
@@ -1,11 +1,12 @@
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Lombiq.HelpfulExtensions.Services
 {
     /// <summary>
-    /// This service does nothing. However if you inject <c>IEnumerable&lt;ITestingMarkerService&gt;
-    /// testingMarkerServices</c> to your service implementation, you will be able to do <c>_isTesting =
-    /// testingMarkerServices.Any();</c> to tell if the testing feature is enabled.
+    /// This service does nothing. However if you inject <see cref="IEnumerable{ITestingMarkerService}"/> to your
+    /// service implementation, you will be able to do <c>_isTesting = testingMarkerServices.Any();</c> to tell if the
+    /// testing feature is enabled.
     /// </summary>
     [SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "Necessary marker interface.")]
     public interface ITestingMarkerService
