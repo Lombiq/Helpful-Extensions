d7e3fccfac31299387c92213c03281856ed18de4
Oliver Friedrich <oliver.friedrich@lombiq.com>
2021-06-09 13:31:48 +0200

    Adding extension method to convert int to string using the InvariantCulture

---
+++ Extensions/InvariantExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    public static class InvariantExtensions
+    {
+        /// <summary>
+        /// Returns the string representation of the given <paramref name="value"/> using the <see
+        /// cref="CultureInfo.InvariantCulture"/>.
+        /// </summary>
+        /// <param name="value">The value to convert to a string.</param>
+        /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
+        public static string ToInvariantString(this int value)
+        {
+            return value.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
