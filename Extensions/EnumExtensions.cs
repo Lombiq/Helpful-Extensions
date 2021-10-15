5bc238937e354b3e88095859950e7b134ccd323e
Milán Keszthelyi <milan.keszthelyi@lombiq.com>
2021-10-15 20:11:51 +0200

    Adding GetEnumDisplayName Enum Extension method

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace System
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumDisplayName(this Enum enumValue) =>
+            enumValue.GetType()?
+                .GetMember(enumValue.ToString())?
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()?
+                .Name;
+    }
+}
