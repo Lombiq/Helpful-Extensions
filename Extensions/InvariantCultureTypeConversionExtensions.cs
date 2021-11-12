fa2598a969ade26bcb529e56ffabc67d0bcb7d35
Bence Puschmann <bence.puschmann@lombiq.com>
2021-11-12 21:18:23 +0100

    Creating ToStringInvariant() for DateTime nullable

--- Extensions/InvariantCultureTypeConversionExtensions.cs
+++ Extensions/InvariantCultureTypeConversionExtensions.cs
@@ -20,5 +20,14 @@ namespace System
         /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
         public static string ToInvariantString(this DateTime value, string format) => 
             value.ToString(format, CultureInfo.InvariantCulture);
+
+        /// <summary>
+        /// Returns the string representation of the given <paramref name="value"/> using the <see
+        /// cref="CultureInfo.InvariantCulture"/>.
+        /// </summary>
+        /// <param name="value">The value to convert to a string.</param>
+        /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
+        public static string ToInvariantString(this DateTime? value) =>
+            !value.HasValue ? string.Empty : value.Value.ToString(CultureInfo.InvariantCulture);
     }
 }
