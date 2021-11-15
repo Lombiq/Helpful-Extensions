243b9f0bb21a383ee785837ab36554cdfa798894
Bence Puschmann <bence.puschmann@lombiq.com>
2021-11-15 15:40:06 +0100

    Simplifying ToInvariantString

--- Extensions/InvariantCultureTypeConversionExtensions.cs
+++ Extensions/InvariantCultureTypeConversionExtensions.cs
@@ -28,6 +28,15 @@ namespace System
         /// <param name="value">The value to convert to a string.</param>
         /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
         public static string ToInvariantString(this DateTime? value) =>
-            !value.HasValue ? string.Empty : value.Value.ToString(CultureInfo.InvariantCulture);
+            value?.ToString(CultureInfo.InvariantCulture) ?? string.Empty;
+
+        /// <summary>
+        /// Returns the string representation of the given <paramref name="value"/> using the <see
+        /// cref="CultureInfo.InvariantCulture"/>.
+        /// </summary>
+        /// <param name="value">The value to convert to a string.</param>
+        /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
+        public static string ToInvariantString(this DateTime? value, string format) =>
+            value?.ToInvariantString(format) ?? string.Empty;
     }
 }
