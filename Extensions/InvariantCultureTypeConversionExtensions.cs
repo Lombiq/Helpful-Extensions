b41c334845f37757f57e6ff01da6591a3b7f1746
Milán Keszthelyi <milan.keszthelyi@lombiq.com>
2021-11-16 14:08:11 +0100

    Adding ToInvariantString for DateTimes without the format parameter too

--- Extensions/InvariantCultureTypeConversionExtensions.cs
+++ Extensions/InvariantCultureTypeConversionExtensions.cs
@@ -18,8 +18,8 @@ namespace System
         /// </summary>
         /// <param name="value">The value to convert to a string.</param>
         /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
-        public static string ToInvariantString(this DateTime value, string format) => 
-            value.ToString(format, CultureInfo.InvariantCulture);
+        public static string ToInvariantString(this DateTime value) =>
+            value.ToString(CultureInfo.InvariantCulture);
 
         /// <summary>
         /// Returns the string representation of the given <paramref name="value"/> using the <see
@@ -28,7 +28,16 @@ namespace System
         /// <param name="value">The value to convert to a string.</param>
         /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
         public static string ToInvariantString(this DateTime? value) =>
-            value?.ToString(CultureInfo.InvariantCulture) ?? string.Empty;
+            value?.ToInvariantString() ?? string.Empty;
+
+        /// <summary>
+        /// Returns the string representation of the given <paramref name="value"/> using the <see
+        /// cref="CultureInfo.InvariantCulture"/>.
+        /// </summary>
+        /// <param name="value">The value to convert to a string.</param>
+        /// <returns>A predictable string representation of the given <paramref name="value"/>.</returns>
+        public static string ToInvariantString(this DateTime value, string format) => 
+            value.ToString(format, CultureInfo.InvariantCulture);
 
         /// <summary>
         /// Returns the string representation of the given <paramref name="value"/> using the <see
