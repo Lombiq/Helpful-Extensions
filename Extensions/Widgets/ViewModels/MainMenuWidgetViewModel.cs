0800d874435062f0abfff92eb0695dc91de838b1
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-21 20:40:22 +0100

    Non-dynamic viewmodel.

--- Extensions/Widgets/ViewModels/MainMenuWidgetViewModel.cs
+++ Extensions/Widgets/ViewModels/MainMenuWidgetViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using OrchardCore.Navigation;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lombiq.HelpfulExtensions.Extensions.Widgets.ViewModels
 {
@@ -7,5 +8,16 @@ namespace Lombiq.HelpfulExtensions.Extensions.Widgets.ViewModels
     {
         public bool NoWrapper { get; set; }
         public IEnumerable<MenuItem> MenuItems { get; set; }
+
+        public MainMenuWidgetViewModel(bool noWrapper = false, IEnumerable<MenuItem> menuItems = null)
+        {
+            NoWrapper = noWrapper;
+            MenuItems = menuItems ?? Enumerable.Empty<MenuItem>();
+        }
+
+        public MainMenuWidgetViewModel(dynamic model)
+            : this(model.NoWrapper as bool? ?? false, model.MenuItems as IEnumerable<MenuItem>)
+        {
+        }
     }
 }
