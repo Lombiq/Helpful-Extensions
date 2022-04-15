5d0c5a9c49ea8aded6565b9b0ded38f7fb05c0cf
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-15 18:04:14 +0200

    Form post menu items.

---
+++ Extensions/Widgets/Constants/RouteValueNames.cs
@@ -0,0 +1,7 @@
+﻿namespace Lombiq.HelpfulExtensions.Extensions.Widgets.Constants;
+
+public class RouteValueNames
+{
+    private const string MenuWidgetRoot = "Lombiq.MenuWidget.";
+    public const string MenuWidgetPost = MenuWidgetRoot + "Post";
+}
