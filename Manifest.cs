56de69f437bc128a5ff26e2fba86727efa835cc3
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-05-06 00:28:36 +0200

    Updating version number

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using static Lombiq.HelpfulExtensions.FeatureIds;
 [assembly: Module(
     Name = "Lombiq Helpful Extensions",
     Author = "Lombiq Technologies",
-    Version = "3.1.0",
+    Version = "3.2.0",
     Website = "https://github.com/Lombiq/Helpful-Extensions"
 )]
 
