eaf521b14ba46fb03053b90d8149d73778d62af3
Tamás Vincze <tamas.vincze@lombiq.com>
2022-05-20 12:43:49 +0200

    Creating Startup

---
+++ Extensions/TargetBlank/Startup.cs
@@ -0,0 +1,31 @@
+using Lombiq.HelpfulExtensions.Extensions.TargetBlank.Filters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Environment.Shell.Configuration;
+using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
+using System;
+
+namespace Lombiq.HelpfulExtensions.Extensions.TargetBlank;
+
+[Feature(FeatureIds.TargetBlank)]
+public class Startup : StartupBase
+{
+    private readonly IShellConfiguration _shellConfiguration;
+
+    public Startup(IShellConfiguration shellConfiguration) => _shellConfiguration = shellConfiguration;
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.Configure<MvcOptions>((options) => options.Filters.Add(typeof(TargetBlankFilter)));
+
+        services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
+    }
+    public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
+        // No need for anything here yet.
+    }
+}
