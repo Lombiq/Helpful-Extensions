76c2b93b7f578cc12d7b23845778d4b51f3b3fa4
Tamás Vincze <tamas.vincze@lombiq.com>
2022-05-23 12:13:03 +0200

    Adding ResourceManagementOptionsConfiguration

---
+++ ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+
+namespace Lombiq.HelpfulExtensions;
+public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+{
+
+    private static readonly ResourceManifest _manifest = new();
+
+    static ResourceManagementOptionsConfiguration() =>
+        _manifest
+            .DefineScript("Lombiq.HelpfulExtensions.TargetBlank")
+            .SetUrl("~/Lombiq.HelpfulExtensions/TargetBlank/TargetBlank.min.js", "~/Lombiq.HelpfulExtensions/TargetBlank/TargetBlank.js");
+
+    public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
+}
