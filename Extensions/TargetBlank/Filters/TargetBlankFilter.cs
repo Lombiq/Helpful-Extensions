b708c7c414d356407a6e68ce64ea38e3821302d0
Tamás Vincze <tamas.vincze@lombiq.com>
2022-05-23 12:14:11 +0200

    Creating TargetBlankFilter

---
+++ Extensions/TargetBlank/Filters/TargetBlankFilter.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using OrchardCore.ResourceManagement;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulExtensions.Extensions.TargetBlank.Filters;
+public class TargetBlankFilter : IAsyncResultFilter
+{
+    private readonly IResourceManager _resourceManager;
+
+    public TargetBlankFilter(
+        IResourceManager resourceManager) => _resourceManager = resourceManager;
+
+    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        if (context.Result is not ViewResult)
+        {
+            await next();
+
+            return;
+        }
+
+        _resourceManager.RegisterResource("script", "Lombiq.HelpfulExtensions.TargetBlank").AtFoot();
+
+        await next();
+    }
+}
