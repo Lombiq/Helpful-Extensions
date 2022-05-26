a7648020e3578d34090f003598ddbe564b1c516b
Tamás Vincze <tamas.vincze@lombiq.com>
2022-05-26 11:04:45 +0200

    Removing timeout

--- Extensions/TargetBlank/Scripts/TargetBlank.js
+++ Extensions/TargetBlank/Scripts/TargetBlank.js
@@ -1,14 +1,16 @@
+document.currentScript.setAttribute('defer', 'defer');
+
 function targetBlank() {
     var x = document.querySelectorAll("a");
 
     for (var i = 0; i < x.length; i++) {
+        console.log(x[i].href);
+
         if (!x[i].href.match(/^mailto\:/) && (x[i].hostname != location.hostname)) {
+
             x[i].setAttribute("target", "_blank");
         }
     }
 }
 
-window.addEventListener("load", function () {
-    window.setTimeout(targetBlank, 100);
-}, false);
-
+targetBlank();
