0d217c47b7c735430cd1b22f3660597614f41b4f
Tamás Vincze <tamas.vincze@lombiq.com>
2022-05-31 15:55:14 +0200

    Fixing quotation marks

--- Extensions/TargetBlank/Scripts/TargetBlank.js
+++ Extensions/TargetBlank/Scripts/TargetBlank.js
@@ -1,14 +1,14 @@
 document.currentScript.setAttribute('defer', 'defer');
 
 function targetBlank() {
-    var x = document.querySelectorAll("a");
+    var x = document.querySelectorAll('a');
 
     for (var i = 0; i < x.length; i++) {
         console.log(x[i].href);
 
         if (!x[i].href.match(/^mailto\:/) && (x[i].hostname != location.hostname)) {
 
-            x[i].setAttribute("target", "_blank");
+            x[i].setAttribute('target', '_blank');
         }
     }
 }
