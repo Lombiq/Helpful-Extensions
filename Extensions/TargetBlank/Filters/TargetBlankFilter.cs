1cf2ce3b11024b73df9dc03370d46c746e99790a
Roland Márkus <roland.markus@lombiq.com>
2022-06-09 14:28:27 +0200

    Added Target Blank feature.

---
+++ Extensions/TargetBlank/Filters/TargetBlankFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using OrchardCore.ResourceManagement;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulExtensions.Extensions.TargetBlank.Filters;
+public class TargetBlankFilter : IAsyncResultFilter
+{
+    private readonly IResourceManager _resourceManager;
+
+    public TargetBlankFilter(
+        IResourceManager resourceManager) => _resourceManager = resourceManager;
+
+    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        if (context.Result is not ViewResult)
+        {
+            await next();
+
+            return;
+        }
+
+        _resourceManager.RegisterFootScript(new HtmlString(
+            "<script>function targetBlank(){const x=document.querySelectorAll('a');" +
+            "for(let i=0;i<x.length;i++){if(!x[i].href.match(/^mailto:/)&&(x[i].hostname!==location.hostname))" +
+            "{x[i].setAttribute('target','_blank')}}}window.addEventListener('load',function()" +
+            "{window.setTimeout(targetBlank,100)},false);</script>"));
+
+        await next();
+    }
+}
