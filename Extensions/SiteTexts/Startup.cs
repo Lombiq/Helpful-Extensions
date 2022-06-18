905910e5d29c5cb835d9021597800faf4b2cf713
Dávid El-Saig <david.el-saig@lombiq.com>
2022-06-18 23:23:04 +0200

    Remove unnecessary service registration.

--- Extensions/SiteTexts/Startup.cs
+++ Extensions/SiteTexts/Startup.cs
@@ -11,8 +11,6 @@ public class Startup : StartupBase
     public override void ConfigureServices(IServiceCollection services)
     {
         services.AddScoped<IDataMigration, Migrations>();
-
-        services.AddScoped<SiteTextService>();
         services.AddScoped<ISiteTextService, SiteTextService>();
     }
 }
