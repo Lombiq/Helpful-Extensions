6291919698526640d5427884fb15ac018d1fd5d3
Roland Márkus <roland.markus@lombiq.com>
2022-06-21 16:10:58 +0200

    Adjusting format.

--- Extensions/TargetBlank/Filters/TargetBlankFilter.cs
+++ Extensions/TargetBlank/Filters/TargetBlankFilter.cs
@@ -21,15 +21,24 @@ public class TargetBlankFilter : IAsyncResultFilter
             return;
         }
 
-        const string script = @"<script>
-                                function targetBlank() {
-                                    const x=document.querySelectorAll('a');
-                                    for(let i=0;i<x.length;i++) {
-                                        if(!x[i].href.match(/^mailto:/)&&(x[i].hostname!==location.hostname&&(!x[i].href.match('javascript:')))) {
-                                            x[i].setAttribute('target','_blank')}}}
-                                window.addEventListener('load',function() {
-                                    window.setTimeout(targetBlank,100)},false);
-                               </script>";
+        const string script = @"
+            <script>
+                    function targetBlank() {
+                        const x=document.querySelectorAll('a');
+                        for(let i=0;i<x.length;i++) {
+                            if(!x[i].href.match(/^mailto:/)&&(x[i].hostname!==location.hostname&&(!x[i].href.match('javascript:'))))
+                            {
+                                x[i].setAttribute('target','_blank')
+                            }
+                        }
+                    }
+                    window.addEventListener(
+                        'load',
+                        function() {
+                            window.setTimeout(targetBlank,100)
+                        },
+                        false);
+            </script>";
 
         // Until Node Extensions is ready for use, this solution is here to replace the usage of Gulp. When Node
         // Extensions is ready, this script should be replaced with a JavaScript file.
