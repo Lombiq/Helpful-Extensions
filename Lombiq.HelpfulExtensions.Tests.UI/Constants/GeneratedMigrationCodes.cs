08e4e83c97ad9e8dd7798f96d783d1cf00522dd8
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-08-22 18:17:25 +0200

    Fixing EOL mismatch under Linux

--- Lombiq.HelpfulExtensions.Tests.UI/Constants/GeneratedMigrationCodes.cs
+++ Lombiq.HelpfulExtensions.Tests.UI/Constants/GeneratedMigrationCodes.cs
@@ -1,33 +1,36 @@
+using System;
+
 namespace Lombiq.HelpfulExtensions.Tests.UI.Constants;
 
 internal static class GeneratedMigrationCodes
 {
-    // The funny indentation is necessary to match the indentations of the textarea.
-    public static string Page = @"_contentDefinitionManager.AlterTypeDefinition(""Page"", type => type
-    .DisplayedAs(""Page"")
-    .Creatable()
-    .Listable()
-    .Draftable()
-    .Versionable()
-    .Securable()
-    .WithPart(""TitlePart"", part => part
-        .WithPosition(""0"")
-    )
-    .WithPart(""AutoroutePart"", part => part
-        .WithPosition(""1"")
-        .WithSettings(new AutoroutePartSettings
-        {
-            AllowCustomPath = true,
-            ShowHomepageOption = true,
-            Pattern = ""{{ Model.ContentItem | display_text | slugify }}"",
-        })
-    )
-    .WithPart(""FlowPart"", part => part
-        .WithPosition(""2"")
-    )
-    .WithPart(""Page"", part => part
-        .WithPosition(""3"")
-    )
-);
-";
+    // Environment.NewLine is used for this to work regardless of the mismatch of line ending style between the code
+    // file, the platform where it was compiled, and where it was executed.
+    public static string Page =
+        "_contentDefinitionManager.AlterTypeDefinition(\"Page\", type => type" + Environment.NewLine +
+        "    .DisplayedAs(\"Page\")" + Environment.NewLine +
+        "    .Creatable()" + Environment.NewLine +
+        "    .Listable()" + Environment.NewLine +
+        "    .Draftable()" + Environment.NewLine +
+        "    .Versionable()" + Environment.NewLine +
+        "    .Securable()" + Environment.NewLine +
+        "    .WithPart(\"TitlePart\", part => part" + Environment.NewLine +
+        "        .WithPosition(\"0\")" + Environment.NewLine +
+        "    )" + Environment.NewLine +
+        "    .WithPart(\"AutoroutePart\", part => part" + Environment.NewLine +
+        "        .WithPosition(\"1\")" + Environment.NewLine +
+        "        .WithSettings(new AutoroutePartSettings" + Environment.NewLine +
+        "        {" + Environment.NewLine +
+        "            AllowCustomPath = true," + Environment.NewLine +
+        "            ShowHomepageOption = true," + Environment.NewLine +
+        "            Pattern = \"{{ Model.ContentItem | display_text | slugify }}\"," + Environment.NewLine +
+        "        })" + Environment.NewLine +
+        "    )" + Environment.NewLine +
+        "    .WithPart(\"FlowPart\", part => part" + Environment.NewLine +
+        "        .WithPosition(\"2\")" + Environment.NewLine +
+        "    )" + Environment.NewLine +
+        "    .WithPart(\"Page\", part => part" + Environment.NewLine +
+        "        .WithPosition(\"3\")" + Environment.NewLine +
+        "    )" + Environment.NewLine +
+        ");" + Environment.NewLine;
 }
