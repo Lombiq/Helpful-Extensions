78c45e65f55c383c88ee16930f46343bf1ae421b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-09-08 21:28:09 +0200

    Trying to get rid of StaleElementReferenceException when testing the code generation feature

--- Lombiq.HelpfulExtensions.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.HelpfulExtensions.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -105,9 +105,13 @@ public static class TestCaseUITestContextExtensions
         await context.EnableFeatureDirectlyAsync(FeatureIds.CodeGeneration);
 
         await context.GoToContentTypeEditorAsync("Page");
-        await context.ClickReliablyOnAsync(By.ClassName("toggle-showing-generated-migration-code"));
+        // Not trying repeated clicks, not to cause the code container to collapse.
+        await context.ClickReliablyOnAsync(By.ClassName("toggle-showing-generated-migration-code"), 1);
 
         context.Get(By.Id("generated-migration-code").OfAnyVisibility()).GetValue().ShouldBe(GeneratedMigrationCodes.Page);
+
+        // Making sure that the code is visible.
+        context.ScrollTo(By.Id("generated-migration-code-container"));
 
         // Checking the first line of the CodeMirror editor.
         context.Get(By.CssSelector(".CodeMirror-line .cm-variable")).Text.ShouldBe("_contentDefinitionManager");
