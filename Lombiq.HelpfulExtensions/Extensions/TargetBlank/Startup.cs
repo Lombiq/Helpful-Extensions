c64febc88ad8d7b88da2e3a317f9b60b80aa8536
Zsolt Takács <zsolt.takacs@lombiq.com>
2023-02-16 07:15:58 +0100

    Adding Constant for future use, adding dependencies, importing NodeJs.Extensions, setting the path of the .js file

--- Lombiq.HelpfulExtensions/Extensions/TargetBlank/Startup.cs
+++ Lombiq.HelpfulExtensions/Extensions/TargetBlank/Startup.cs
@@ -1,9 +1,11 @@
-﻿using Lombiq.HelpfulExtensions.Extensions.TargetBlank.Filters;
+using Lombiq.HelpfulExtensions.Extensions.TargetBlank.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
 using System;
 
 namespace Lombiq.HelpfulExtensions.Extensions.TargetBlank;
@@ -11,8 +13,11 @@ namespace Lombiq.HelpfulExtensions.Extensions.TargetBlank;
 [Feature(FeatureIds.TargetBlank)]
 public class Startup : StartupBase
 {
-    public override void ConfigureServices(IServiceCollection services) =>
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
         services.Configure<MvcOptions>(options => options.Filters.Add(typeof(TargetBlankFilter)));
+    }
 
     public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
     {
---
+++ Lombiq.HelpfulExtensions/ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+using static Lombiq.HelpfulExtensions.Constants.ResourceNames;
+
+namespace Lombiq.HelpfulExtensions;
+
+public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+{
+    private const string WwwRoot = "~/Lombiq.HelpfulExtensions/";
+    private static readonly ResourceManifest _manifest = new();
+
+    static ResourceManagementOptionsConfiguration() =>
+        _manifest
+            .DefineScript(TargetBlank)
+            .SetUrl(WwwRoot + "scripts/target-blank.min.js", WwwRoot + "scripts/target-blank.js")
+            .SetVersion("1.0.0");
+
+    public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
+}
