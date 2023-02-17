4cdedc77078889b4584550de633972b776c96c3f
Zsolt Takács <zsolt.takacs@lombiq.com>
2023-02-17 22:00:34 +0100

    iife

--- Lombiq.HelpfulExtensions/Assets/Scripts/target-blank.js
+++ Lombiq.HelpfulExtensions/Assets/Scripts/target-blank.js
@@ -1,17 +1,19 @@
-function targetBlank() {
-    const links = document.querySelectorAll('a');
-    const currentHostname = window.location.hostname;
+(function () {
+    function targetBlank() {
+        const links = document.querySelectorAll('a');
+        const currentHostname = window.location.hostname;
 
-    for (let i = 0; i < links.length; i++) {
-        if (!links[i].href.match(/^mailto:/) &&
-            (links[i].hostname !== currentHostname && (!links[i].href.match(/^javascript:/i)))) {
-            links[i].setAttribute('target', '_blank');
+        for (let i = 0; i < links.length; i++) {
+            if (!links[i].href.match(/^mailto:/) &&
+                (links[i].hostname !== currentHostname && (!links[i].href.match(/^javascript:/i)))) {
+                links[i].setAttribute('target', '_blank');
+            }
         }
     }
-}
-window.addEventListener(
-    'load',
-    () => {
-        window.setTimeout(targetBlank, 100);
-    },
-    false);
+    window.addEventListener(
+        'load',
+        () => {
+            window.setTimeout(targetBlank, 100);
+        },
+        false);
+})();
