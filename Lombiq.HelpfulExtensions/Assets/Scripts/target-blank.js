d93dd871c1bf985e0a302a061f495da2b500db18
Zsolt Takács <zsolt.takacs@lombiq.com>
2023-02-20 11:27:29 +0100

    Removing superfluous parentheses and fixing EventListener

--- Lombiq.HelpfulExtensions/Assets/Scripts/target-blank.js
+++ Lombiq.HelpfulExtensions/Assets/Scripts/target-blank.js
@@ -5,16 +5,11 @@
 
         for (let i = 0; i < links.length; i++) {
             if (!links[i].href.match(/^mailto:/) &&
-                (links[i].hostname !== currentHostname &&
-                (!links[i].href.match(/^javascript:/i)))) {
+                links[i].hostname !== currentHostname &&
+                !links[i].href.match(/^javascript:/i)) {
                 links[i].setAttribute('target', '_blank');
             }
         }
     }
-    window.addEventListener(
-        'load',
-        () => {
-            window.setTimeout(targetBlank, 100);
-        },
-        false);
+    document.addEventListener('DOMContentLoaded', targetBlank);
 })();
