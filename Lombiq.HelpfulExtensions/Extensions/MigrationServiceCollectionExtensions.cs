8b1e4c37e1eecb5473f2ff1b08035190edc1f432
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-02-20 18:33:34 +0100

    Code styling

--- Lombiq.HelpfulExtensions/Extensions/MigrationServiceCollectionExtensions.cs
+++ Lombiq.HelpfulExtensions/Extensions/MigrationServiceCollectionExtensions.cs
@@ -3,9 +3,10 @@ using OrchardCore.Data.Migration;
 
 namespace Lombiq.HelpfulExtensions.Extensions;
 
-/// We need to use this until the Orchard Core 1.6 upgrade so we don't have to upgrade submodules to
-/// to nightly versions. This is the same method as what is in the 1.6 nightly version of Orchard Core.
-/// During the upgrade, this can be removed and use the Orchard Core method for data migrations.
+// We need to use this until the Orchard Core upgrade so we don't have to upgrade submodules to nightly versions. This
+// is the same method as what is in the 1.6 nightly version of Orchard Core. During the upgrade, this can be removed and
+// use the Orchard Core method for data migrations.
+
 /// <summary>
 /// Provides extension methods for <see cref="IServiceCollection"/> to add YesSql migration <see cref="IDataMigration"/>.
 /// </summary>
