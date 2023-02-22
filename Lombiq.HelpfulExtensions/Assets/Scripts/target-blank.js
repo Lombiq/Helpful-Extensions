dbbe57eb25df806a0244b96e70e641375674b0ed
Zsolt Takács <zsolt.takacs@lombiq.com>
2023-02-22 15:43:00 +0100

    Adding comment to explain eslint-disable-next-line

--- Lombiq.HelpfulExtensions/Assets/Scripts/target-blank.js
+++ Lombiq.HelpfulExtensions/Assets/Scripts/target-blank.js
@@ -5,6 +5,8 @@
 
         for (let i = 0; i < links.length; i++) {
             if (links[i].hostname !== currentHostname &&
+                // The no-script-url rule triggers to prevent the usage of javascript: URLs.
+                // In the current script, we are not using them.
                 // eslint-disable-next-line no-script-url
                 !links[i].href.startsWith('javascript:') &&
                 !links[i].href.startsWith('mailto:')) {
