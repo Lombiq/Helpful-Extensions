4a7073f744fbf976dfc7d1d607e6c53f32db430d
Dávid El-Saig <david.el-saig@lombiq.com>
2023-02-23 19:09:53 +0100

    Add startup.

---
+++ Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Startup.cs
@@ -0,0 +1,16 @@
+using LombiqDotCom.Services;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace Lombiq.HelpfulExtensions.Extensions.OrchardRecipeMigration;
+
+[Feature(FeatureIds.OrchardRecipeMigration)]
+public class Startup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<IOrchardExportToRecipeConverter, OrchardExportToRecipeConverter>();
+        services.AddScoped<IOrchardContentConverter, CommonOrchardContentConverter>();
+        services.AddScoped<IOrchardContentConverter, GraphMetadataOrchardContentConverter>();
+    }
+}
