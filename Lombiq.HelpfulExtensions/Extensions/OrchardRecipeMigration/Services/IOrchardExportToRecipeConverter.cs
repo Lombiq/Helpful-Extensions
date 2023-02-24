ed7962d709fc56434f185feab667c280968c0fe1
Dávid El-Saig <david.el-saig@lombiq.com>
2023-02-24 06:57:08 +0100

    Base service to convert content picker.

--- Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Services/IOrchardExportToRecipeConverter.cs
+++ Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Services/IOrchardExportToRecipeConverter.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace LombiqDotCom.Services;
@@ -7,13 +6,4 @@ namespace LombiqDotCom.Services;
 public interface IOrchardExportToRecipeConverter
 {
     Task<string> ConvertAsync(XDocument export);
-}
-
-public static class OrchardExportToRecipeConverterExtensions
-{
-    public static async Task<string> ConvertAsync(this IOrchardExportToRecipeConverter converter, IFormFile file)
-    {
-        await using var stream = file.OpenReadStream();
-        return await converter.ConvertAsync(XDocument.Load(stream));
-    }
 }
