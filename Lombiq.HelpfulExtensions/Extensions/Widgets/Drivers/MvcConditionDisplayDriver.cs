3d3be69c3b08b5258b30faf48b03a4a3b60a7bbf
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-14 15:11:50 +0200

    MVC condition.

---
+++ Lombiq.HelpfulExtensions/Extensions/Widgets/Drivers/MvcConditionDisplayDriver.cs
@@ -0,0 +1,91 @@
+﻿using Lombiq.HelpfulExtensions.Extensions.Widgets.Models;
+using Lombiq.HelpfulExtensions.Extensions.Widgets.ViewModels;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Views;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lombiq.HelpfulExtensions.Extensions.Widgets.Drivers;
+
+public class MvcConditionDisplayDriver : ConditionDisplayDriver<MvcCondition>
+{
+    private readonly IHtmlLocalizer<MvcConditionDisplayDriver> H;
+    private readonly IStringLocalizer<MvcConditionDisplayDriver> T;
+    public MvcConditionDisplayDriver(
+        IHtmlLocalizer<MvcConditionDisplayDriver> htmlLocalizer,
+        IStringLocalizer<MvcConditionDisplayDriver> stringLocalizer)
+    {
+        H = htmlLocalizer;
+        T = stringLocalizer;
+    }
+
+    protected override IDisplayResult GetEditor(MvcCondition model) =>
+        Initialize<MvcConditionViewModel>("MvcCondition_Fields_Edit", viewModel =>
+        {
+            viewModel.Area = model.Area;
+            viewModel.Controller = model.Controller;
+            viewModel.Action = model.Action;
+
+            foreach (var (key, value) in model.OtherRouteValues)
+            {
+                viewModel.OtherRouteNames.Add(key);
+                viewModel.OtherRouteValues.Add(value);
+            }
+        }).PlaceInContent();
+
+    public override async Task<IDisplayResult> UpdateAsync(MvcCondition model, IUpdateModel updater)
+    {
+        var viewModel = new MvcConditionViewModel();
+        if (await updater.TryUpdateModelAsync(viewModel, Prefix))
+        {
+            if (viewModel.OtherRouteNames.Count != viewModel.OtherRouteValues.Count)
+            {
+                updater.ModelState.AddModelError(
+                    nameof(viewModel.OtherRouteNames),
+                    T["The count of other route value names didn't match the count of other route values."]);
+            }
+
+            model.Area = viewModel.Area;
+            model.Controller = viewModel.Controller;
+            model.Action = viewModel.Action;
+
+            model.OtherRouteValues.Clear();
+            for (var i = 0; i < viewModel.OtherRouteNames.Count; i++)
+            {
+                model.OtherRouteValues[viewModel.OtherRouteNames[i]] = viewModel.OtherRouteValues[i];
+            }
+        }
+
+        return Edit(model);
+    }
+
+    protected override ConditionViewModel GetConditionViewModel(MvcCondition condition)
+    {
+        IHtmlContentBuilder summaryHint = new HtmlContentBuilder();
+
+        static IHtmlContentBuilder AppendIfNotEmpty(IHtmlContentBuilder summaryHint, string value, IHtmlContent label) =>
+            string.IsNullOrEmpty(value) ? summaryHint : summaryHint.AppendHtml(label).AppendHtml(" ");
+
+        summaryHint = AppendIfNotEmpty(summaryHint, condition.Area, H["Area: \"{0}\"", condition.Area]);
+        summaryHint = AppendIfNotEmpty(summaryHint, condition.Controller, H["Controller: \"{0}\"", condition.Controller]);
+        summaryHint = AppendIfNotEmpty(summaryHint, condition.Action, H["Action: \"{0}\"", condition.Action]);
+
+        if (condition.OtherRouteValues.Any())
+        {
+            summaryHint = summaryHint.AppendHtml(
+                H["Other route values: {0}", JsonConvert.SerializeObject(condition.OtherRouteValues)]);
+        }
+
+        return new ConditionViewModel
+        {
+            Title = H["MVC condition"],
+            Description = H["An MVC condition evaluates the currently route values such as controller and action name."],
+            SummaryHint = summaryHint,
+        };
+    }
+}
---
+++ Lombiq.HelpfulExtensions/Extensions/Widgets/Models/MvcCondition.cs
@@ -0,0 +1,12 @@
+﻿using OrchardCore.Rules;
+using System.Collections.Generic;
+
+namespace Lombiq.HelpfulExtensions.Extensions.Widgets.Models;
+
+public class MvcCondition : Condition
+{
+    public string Area { get; set; }
+    public string Controller { get; set; }
+    public string Action { get; set; }
+    public IDictionary<string, string> OtherRouteValues { get; } = new Dictionary<string, string>();
+}
