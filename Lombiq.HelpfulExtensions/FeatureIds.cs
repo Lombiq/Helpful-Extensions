28c8a7775d1b1f90e88d84095966c10e6e1e153d
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-27 05:52:29 +0200

    Add new feature.

--- Lombiq.HelpfulExtensions/FeatureIds.cs
+++ Lombiq.HelpfulExtensions/FeatureIds.cs
@@ -5,6 +5,7 @@ public static class FeatureIds
     private const string FeatureIdPrefix = "Lombiq.HelpfulExtensions.";
 
     public const string CodeGeneration = FeatureIdPrefix + nameof(CodeGeneration);
+    public const string ContentSets = FeatureIdPrefix + nameof(ContentSets);
     public const string ContentTypes = FeatureIdPrefix + nameof(ContentTypes);
     public const string Flows = FeatureIdPrefix + nameof(Flows);
     public const string ShapeTracing = FeatureIdPrefix + nameof(ShapeTracing);
