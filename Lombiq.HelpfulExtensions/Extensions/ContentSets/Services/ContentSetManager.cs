cc0b78d37d64db37dfbb2b5de651a12003c287f6
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-27 06:27:11 +0200

    manager method implementation.

--- Lombiq.HelpfulExtensions/Extensions/ContentSets/Services/ContentSetManager.cs
+++ Lombiq.HelpfulExtensions/Extensions/ContentSets/Services/ContentSetManager.cs
@@ -1,18 +1,26 @@
-﻿using OrchardCore.ContentManagement;
+﻿using Lombiq.HelpfulExtensions.Extensions.ContentSets.Indexes;
+using OrchardCore.ContentManagement;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using YesSql;
 
 namespace Lombiq.HelpfulExtensions.Extensions.ContentSets.Services;
 
 public class ContentSetManager : IContentSetManager
 {
     private readonly IContentManager _contentManager;
+    private readonly ISession _session;
 
-    public ContentSetManager(IContentManager contentManager) =>
+    public ContentSetManager(IContentManager contentManager, ISession session)
+    {
         _contentManager = contentManager;
+        _session = session;
+    }
 
-    public Task<IEnumerable<string>> GetContentItemIdsAsync(string setId) =>
-        throw new System.NotImplementedException();
+    public async Task<IEnumerable<string>> GetContentItemIdsAsync(string setId) =>
+        (await _session.QueryIndex<ContentSetIndex>(index => index.ContentSet == setId).ListAsync())
+            .Select(index => index.ContentItemId);
 
     public async Task<IEnumerable<ContentItem>> GetContentItemsAsync(string setId) =>
         await _contentManager.GetAsync(await GetContentItemIdsAsync(setId));
