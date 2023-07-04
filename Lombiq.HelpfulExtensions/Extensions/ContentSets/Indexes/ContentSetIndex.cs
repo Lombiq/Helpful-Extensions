8064f294142ed4e8a27627d56e1cf4c3968aee1c
Dávid El-Saig <david.el-saig@lombiq.com>
2023-07-04 22:28:01 +0200

    Fix index.

--- Lombiq.HelpfulExtensions/Extensions/ContentSets/Indexes/ContentSetIndex.cs
+++ Lombiq.HelpfulExtensions/Extensions/ContentSets/Indexes/ContentSetIndex.cs
@@ -1,4 +1,5 @@
 ﻿using Lombiq.HelpfulExtensions.Extensions.ContentSets.Models;
+using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata;
 using System;
@@ -28,20 +29,27 @@ public class ContentSetIndex : MapIndex
 
 public class ContentSetIndexProvider : IndexProvider<ContentItem>
 {
-    private readonly Lazy<IContentDefinitionManager> _contentDefinitionManager;
+    private readonly IServiceProvider _provider;
 
-    public ContentSetIndexProvider(Lazy<IContentDefinitionManager> contentDefinitionManager) =>
-        _contentDefinitionManager = contentDefinitionManager;
+    // We can't inject Lazy<IContentDefinitionManager> because it will throw a "Cannot resolve scoped service
+    // 'OrchardCore.ContentManagement.Metadata.IContentDefinitionManager' from root provider." exception.
+    public ContentSetIndexProvider(IServiceProvider provider) =>
+        _provider = provider;
 
     public override void Describe(DescribeContext<ContentItem> context) =>
-        context.For<ContentSetIndex>().Map(contentItem => !contentItem.Latest
-            ? Enumerable.Empty<ContentSetIndex>()
-            : _contentDefinitionManager
-                .Value
-                .GetTypeDefinition(contentItem.ContentType)
-                .Parts
-                .Where(part => part.PartDefinition.Name == nameof(ContentSetPart))
-                .Select(part => new { Part = contentItem.Get<ContentSetPart>(part.Name), part.Name })
-                .Where(info => info.Part != null)
-                .Select(info => ContentSetIndex.FromPart(info.Part, info.Name)));
+        context.For<ContentSetIndex>().Map(contentItem =>
+        {
+            if (!contentItem.Latest) return Enumerable.Empty<ContentSetIndex>();
+
+            using var scope = _provider.CreateScope();
+            var contentDefinitionManager = scope.ServiceProvider.GetRequiredService<IContentDefinitionManager>();
+
+            return contentDefinitionManager
+                    .GetTypeDefinition(contentItem.ContentType)
+                    .Parts
+                    .Where(part => part.PartDefinition.Name == nameof(ContentSetPart))
+                    .Select(part => new { Part = contentItem.Get<ContentSetPart>(part.Name), part.Name })
+                    .Where(info => info.Part != null)
+                    .Select(info => ContentSetIndex.FromPart(info.Part, info.Name));
+        });
 }
