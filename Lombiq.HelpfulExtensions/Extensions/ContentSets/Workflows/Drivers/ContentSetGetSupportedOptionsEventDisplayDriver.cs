2b9999ff4c496d93ba7a77d486e3ddf5b20a6ed1
Dávid El-Saig <david.el-saig@lombiq.com>
2023-09-08 13:19:28 +0200

    Use DocumentedEventActivityDisplayDriverBase.

--- Lombiq.HelpfulExtensions/Extensions/ContentSets/Workflows/Drivers/ContentSetGetSupportedOptionsEventDisplayDriver.cs
+++ Lombiq.HelpfulExtensions/Extensions/ContentSets/Workflows/Drivers/ContentSetGetSupportedOptionsEventDisplayDriver.cs
@@ -2,41 +2,46 @@
 using Lombiq.HelpfulExtensions.Extensions.ContentSets.ViewModels;
 using Lombiq.HelpfulExtensions.Extensions.ContentSets.Workflows.Activities;
 using Lombiq.HelpfulLibraries.OrchardCore.Workflow;
-using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
 using Newtonsoft.Json;
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Metadata.Models;
+using OrchardCore.DisplayManagement.Notify;
+using System.Collections.Generic;
 
 namespace Lombiq.HelpfulExtensions.Extensions.ContentSets.Workflows.Drivers;
 
-public class ContentSetGetSupportedOptionsEventDisplayDriver : SimpleEventActivityDisplayDriverBase<ContentSetGetSupportedOptionsEvent>
+public class ContentSetGetSupportedOptionsEventDisplayDriver :
+    DocumentedEventActivityDisplayDriverBase<ContentSetGetSupportedOptionsEvent>
 {
     private readonly IHtmlLocalizer<ContentSetGetSupportedOptionsEventDisplayDriver> H;
 
     public override string IconClass => "fa-circle-half-stroke";
 
-    public override LocalizedHtmlString Description
+    public override LocalizedHtmlString Description =>
+        H["Tries to get a list of links representing the supported options for this content set."];
+
+    public override IDictionary<string, string> AvailableInputs { get; } = new Dictionary<string, string>
     {
-        get
-        {
-            var foreword = H["Tries to get a list of links representing the supported options for this content set."];
+        [nameof(GetSupportedOptionsContext.Definition)] = nameof(ContentTypePartDefinition),
+        [nameof(GetSupportedOptionsContext.ContentSetPart)] = nameof(ContentSetPart),
+        [nameof(GetSupportedOptionsContext.ContentItem)] = nameof(ContentItem),
+    };
 
-            var inputKeys = new GetSupportedOptionsContext(Definition: null, ContentSetPart: null).ToDictionary().Keys;
-            var inputs = H["The available inputs are: {0}", string.Join(", ", inputKeys)];
-
-            // Not using Newtonsoft.Json.Schema to generate a real JSON schema due to licensing issues. On the other hand
-            // System.Text.Json doesn't officially support JSON schema generation yet, so that's not an option either.
-            var schema = JsonConvert.SerializeObject(new ContentSetLinkViewModel(
-                IsDeleted: false,
-                "string",
-                "string",
-                "string")) + "[]";
-            var expectation = H["Expects an output \"{0}\" in the format <code>{1}</code>.", ContentSetGetSupportedOptionsEvent.OutputName, schema];
-
-            return new HtmlString("<br>").Join(foreword, inputs, expectation);
-        }
-    }
+    public override IDictionary<string, string> ExpectedOutputs { get; } = new Dictionary<string, string>
+    {
+        [ContentSetGetSupportedOptionsEvent.OutputName] = JsonConvert.SerializeObject(new ContentSetLinkViewModel(
+            IsDeleted: false,
+            "string",
+            "string",
+            "string")) + "[]",
+    };
 
     public ContentSetGetSupportedOptionsEventDisplayDriver(
-        IHtmlLocalizer<ContentSetGetSupportedOptionsEventDisplayDriver> htmlLocalizer) =>
+        INotifier notifier,
+        IStringLocalizer<DocumentedEventActivityDisplayDriver> baseLocalizer,
+        IHtmlLocalizer<ContentSetGetSupportedOptionsEventDisplayDriver> htmlLocalizer)
+        : base(notifier, baseLocalizer) =>
         H = htmlLocalizer;
 }
