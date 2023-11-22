4eef637401a1ba7ac1f1a34c08ad9c4d66170cd5
Ádám Jakab <adam.jakab@lombiq.com>
2023-11-22 19:08:37 +0100

    Improving ToIsoDateString and ToIsoTimeString extension methods

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -10,13 +10,11 @@ namespace System
 
         public static string ToIsoDateString(this DateTime dateTime) => dateTime.ToString("yyyy-MM-dd");
 
-        public static string ToIsoDateString(this DateTime? dateTime) =>
-            dateTime.HasValue ? dateTime.Value.ToIsoDateString() : string.Empty;
+        public static string ToIsoDateString(this DateTime? dateTime) => dateTime?.ToIsoDateString() ?? string.Empty;
 
         public static string ToIsoTimeString(this DateTime dateTime) => dateTime.ToString("HH:mm:ss");
 
-        public static string ToIsoTimeString(this DateTime? dateTime) =>
-            dateTime.HasValue ? dateTime.Value.ToIsoTimeString() : string.Empty;
+        public static string ToIsoTimeString(this DateTime? dateTime) => dateTime?.ToIsoTimeString() ?? string.Empty;
 
         public static DateTime FirstDayOfMonth(this DateTime dateTime) =>
             new DateTime(dateTime.Year, dateTime.Month, 1, 0, 0, 0, dateTime.Kind);
