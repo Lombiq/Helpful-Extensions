90a7ec00121d480ef8c7b18d591d81a9abedcea8
Ádám Jakab <adam.jakab@lombiq.com>
2024-01-09 16:42:20 +0100

    Modifying extension methods to use CultureInfo.InvariantCulture

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -10,11 +10,13 @@ namespace System
 
         public static string ToIsoDateString(this DateTime dateTime) => dateTime.ToString("yyyy-MM-dd");
 
-        public static string ToIsoDateString(this DateTime? dateTime) => dateTime?.ToIsoDateString() ?? string.Empty;
+        public static string ToIsoDateInvariantString(this DateTime dateTime) => dateTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
 
-        public static string ToIsoTimeString(this DateTime dateTime) => dateTime.ToString("HH:mm:ss");
+        public static string ToIsoDateInvariantString(this DateTime? dateTime) => dateTime?.ToIsoDateInvariantString() ?? string.Empty;
 
-        public static string ToIsoTimeString(this DateTime? dateTime) => dateTime?.ToIsoTimeString() ?? string.Empty;
+        public static string ToIsoTimeInvariantString(this DateTime dateTime) => dateTime.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+
+        public static string ToIsoTimeInvariantString(this DateTime? dateTime) => dateTime?.ToIsoTimeInvariantString() ?? string.Empty;
 
         public static DateTime FirstDayOfMonth(this DateTime dateTime) =>
             new DateTime(dateTime.Year, dateTime.Month, 1, 0, 0, 0, dateTime.Kind);
