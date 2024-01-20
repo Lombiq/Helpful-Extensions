425acc04c9dc7ea3408300c13dce45fdf52f24a2
Roland Márkus <roland.markus@lombiq.com>
2024-01-20 13:19:56 +0100

    Addressing warnings.

--- Lombiq.HelpfulExtensions/Extensions/Security/Services/StrictSecurityPermissionAuthorizationHandler.cs
+++ Lombiq.HelpfulExtensions/Extensions/Security/Services/StrictSecurityPermissionAuthorizationHandler.cs
@@ -14,7 +14,8 @@ using System.Threading.Tasks;
 namespace Lombiq.HelpfulExtensions.Extensions.Security.Services;
 
 [RequireFeatures(FeatureIds.Security)]
-public class StrictSecurityPermissionAuthorizationHandler(IContentDefinitionManager contentDefinitionManager) : AuthorizationHandler<PermissionRequirement>
+public class StrictSecurityPermissionAuthorizationHandler(IContentDefinitionManager contentDefinitionManager)
+    : AuthorizationHandler<PermissionRequirement>
 {
     private static readonly Dictionary<string, IList<string>> _permissionTemplates = ContentTypePermissionsHelper
         .PermissionTemplates
