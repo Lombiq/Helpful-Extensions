5481622ec68970e14392190240fde39b22c26576
Roland Márkus <roland.markus@lombiq.com>
2024-02-05 15:12:02 +0100

    Removing supresses.

--- Lombiq.HelpfulExtensions/Extensions/ContentSets/Models/ContentSetContentPickerField.cs
+++ Lombiq.HelpfulExtensions/Extensions/ContentSets/Models/ContentSetContentPickerField.cs
@@ -2,8 +2,6 @@ using OrchardCore.ContentManagement;
 
 namespace Lombiq.HelpfulExtensions.Extensions.ContentSets.Models;
 
-#pragma warning disable S2094 // Classes should not be empty
 public class ContentSetContentPickerField : ContentField
-#pragma warning restore S2094 // Classes should not be empty
 {
 }
