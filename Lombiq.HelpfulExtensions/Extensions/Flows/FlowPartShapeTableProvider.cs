eadc295ca18248f9fe6aa5489cc4bd388f9d9efc
Sára El-Saig <sara.el-saig@lombiq.com>
2024-05-04 13:59:19 +0200

    Post merge and OC update fixup

--- Lombiq.HelpfulExtensions/Extensions/Flows/FlowPartShapeTableProvider.cs
+++ Lombiq.HelpfulExtensions/Extensions/Flows/FlowPartShapeTableProvider.cs
@@ -1,10 +1,16 @@
 using OrchardCore.DisplayManagement.Descriptors;
+using System.Threading.Tasks;
 
 namespace Lombiq.HelpfulExtensions.Extensions.Flows;
 
 internal sealed class FlowPartShapeTableProvider : IShapeTableProvider
 {
-    public void Discover(ShapeTableBuilder builder) => builder
-        .Describe("FlowPart")
-        .OnDisplaying(displaying => displaying.Shape.Metadata.Alternates.Add("Lombiq_HelpfulExtensions_Flows_FlowPart"));
+    public ValueTask DiscoverAsync(ShapeTableBuilder builder)
+    {
+        builder
+            .Describe("FlowPart")
+            .OnDisplaying(displaying => displaying.Shape.Metadata.Alternates.Add("Lombiq_HelpfulExtensions_Flows_FlowPart"));
+
+        return ValueTask.CompletedTask;
+    }
 }
