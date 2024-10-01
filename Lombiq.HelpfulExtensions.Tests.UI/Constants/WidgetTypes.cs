d4a37c81b574f715e4ecd79fafeb79ad6c3d3c41
Sára El-Saig <sara.el-saig@lombiq.com>
2024-10-01 21:47:44 +0200

    Update test as well.

--- Lombiq.HelpfulExtensions.Tests.UI/Constants/WidgetTypes.cs
+++ Lombiq.HelpfulExtensions.Tests.UI/Constants/WidgetTypes.cs
@@ -5,6 +5,6 @@ public static class WidgetTypes
     public const string Container = nameof(Container);
     public const string Html = nameof(Html);
     public const string Liquid = nameof(Liquid);
-    public const string Menu = nameof(Menu);
+    public const string Menu = "Menu Navigation Provider";
     public const string Markdown = nameof(Markdown);
 }
