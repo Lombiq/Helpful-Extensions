a83858b32a100c20ff848c64fcc63072bb0b148c
Zoltán Máté <zoltan.mate@lombiq.com>
2025-02-20 18:21:11 +0100

    Deleting old code

--- Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Services/OrchardExportToRecipeConverter.cs
+++ Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Services/OrchardExportToRecipeConverter.cs
@@ -92,47 +92,6 @@ public class OrchardExportToRecipeConverter : IOrchardExportToRecipeConverter
         };
     }
 
-    //public async Task<string> ConvertAsync(XDocument export)
-    //{
-    //    var contentItems = new List<ContentItem>();
-    //    var contents = export.XPathSelectElement("//Content")?.Elements() ?? [];
-    //    var contentTypes = (await _contentDefinitionManager.ListTypeDefinitionsAsync())
-    //        .Select(definition => definition.Name)
-    //        .ToList();
-
-    //    foreach (var content in contents)
-    //    {
-    //        if (await CreateContentItemAsync(content, contentTypes) is { } contentItem)
-    //        {
-    //            contentItem.ContentItemId ??= _idGenerator.GenerateUniqueId();
-    //            contentItem.ContentItemVersionId ??= _idGenerator.GenerateUniqueId();
-
-    //            await _contentConverters
-    //                .Where(converter => converter.IsApplicable(content))
-    //                .OrderBy(converter => converter.Order)
-    //                .AwaitEachAsync(converter => converter.ImportAsync(content, contentItem));
-
-    //            contentItems.Add(contentItem);
-    //        }
-    //        else if (content.Name == nameof(User))
-    //        {
-    //            var customUserConverter = _userConverters.FirstOrDefault(converter => converter.IgnoreDefaultConverter);
-    //            var userConverter = customUserConverter ?? _userConverters.First();
-    //            await userConverter.ImportAsync(content);
-    //        }
-    //    }
-
-    //    foreach (var converter in _exportConverters)
-    //    {
-    //        await converter.UpdateContentItemsAsync(export, contentItems);
-    //    }
-
-    //    var recipe = JObject.FromObject(new RecipeDescriptor())!;
-    //    recipe["steps"] = new JsonArray(JObject.FromObject(new { name = "content", data = contentItems }));
-
-    //    return recipe.ToString();
-    //}
-
     private async Task<ContentItem> CreateContentItemAsync(XElement content, List<string> contentTypes)
     {
         foreach (var converter in _contentConverters.OrderBy(converter => converter.Order))
