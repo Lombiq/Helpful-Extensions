967daa287ab1376140844c502bf74542389453bc
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-08 07:06:41 +0200

    JS cleanup.

--- Lombiq.HelpfulExtensions/wwwroot/js/target-blank.js
+++ Lombiq.HelpfulExtensions/wwwroot/js/target-blank.js
@@ -1,19 +1,16 @@
 (function initTargetBlank() {
     function addTargetBlank() {
-        const links = document.querySelectorAll('a');
+        const links = Array.from(document.querySelectorAll('a'));
         const currentHostname = window.location.hostname;
 
-        for (let i = 0; i < links.length; i++) {
-            if (links[i].hostname !== currentHostname &&
-                // The no-script-url rule triggers to prevent the usage of javascript: URLs.
-                // In the current script, we are not using them.
-                // eslint-disable-next-line no-script-url
-                !links[i].href.startsWith('javascript:') &&
-                !links[i].href.startsWith('mailto:') &&
-                links[i].href) {
-                links[i].setAttribute('target', '_blank');
-            }
-        }
+        links
+            .filter(link => link.hostname !== currentHostname)
+            // The no-script-url rule triggers to prevent the usage of javascript: URLs.
+            // In the current script, we are not using them.
+            // eslint-disable-next-line no-script-url
+            .filter(link => link.href && !link.href.startsWith('javascript:') && !link.href.startsWith('mailto:'))
+            .forEach(link => link.setAttribute('target', '_blank'));
     }
+
     document.addEventListener('DOMContentLoaded', addTargetBlank);
 })();
