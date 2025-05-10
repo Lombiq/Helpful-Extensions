36b14cbfa3e1c64997508d68d62225c160dc035b
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-10 17:48:06 +0200

    Fix "Expected parentheses around arrow function argument".

--- Lombiq.HelpfulExtensions/wwwroot/js/target-blank.js
+++ Lombiq.HelpfulExtensions/wwwroot/js/target-blank.js
@@ -4,12 +4,12 @@
         const currentHostname = window.location.hostname;
 
         links
-            .filter(link => link.hostname !== currentHostname)
+            .filter((link) => link.hostname !== currentHostname)
             // The no-script-url rule triggers to prevent the usage of javascript: URLs.
             // In the current script, we are not using them.
             // eslint-disable-next-line no-script-url
-            .filter(link => link.href && !link.href.startsWith('javascript:') && !link.href.startsWith('mailto:'))
-            .forEach(link => link.setAttribute('target', '_blank'));
+            .filter((link) => link.href && !link.href.startsWith('javascript:') && !link.href.startsWith('mailto:'))
+            .forEach((link) => link.setAttribute('target', '_blank'));
     }
 
     document.addEventListener('DOMContentLoaded', addTargetBlank);
