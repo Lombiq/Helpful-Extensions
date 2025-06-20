5aedae41351b517f5d36daea1299a5725fd35ea5
Pjer Flajhar <pjer.flajhar@lombiq.com>
2025-06-20 22:17:36 +0200

    Fixing errors in AdminMenu.cs

--- Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Navigation/AdminMenu.cs
+++ Lombiq.HelpfulExtensions/Extensions/OrchardRecipeMigration/Navigation/AdminMenu.cs
@@ -1,20 +1,23 @@
 using Lombiq.HelpfulExtensions.Extensions.OrchardRecipeMigration.Controllers;
+using Lombiq.HelpfulLibraries.OrchardCore.Navigation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using OrchardCore.Navigation;
 
 namespace Lombiq.HelpfulExtensions.Extensions.OrchardRecipeMigration.Navigation;
 
 public sealed class AdminMenu : AdminMenuNavigationProviderBase
 {
-
     public AdminMenu(IHttpContextAccessor hca, IStringLocalizer<AdminMenu> stringLocalizer)
         : base(hca, stringLocalizer)
     {
     }
-    protected override void Build(NavigationBuilder builder)
-    {
-        builder.Add(_t["Configuration"], config => config
-            .Add(_t["Import/Export"], section => section
-                .Add(_t["Orchard 1 Recipe Migration"], _t["Orchard 1 Recipe Migration"], item => item
+
+    protected override void Build(NavigationBuilder builder) =>
+        builder.Add(T["Configuration"], config => config
+            .Add(T["Import/Export"], section => section
+                .Add(T["Orchard 1 Recipe Migration"], T["Orchard 1 Recipe Migration"], item => item
                     .Action<OrchardRecipeMigrationAdminController>(_hca.HttpContext, controller => controller.Index())
-                    .LocalNav())));
-    }
+                    .LocalNav()
+                )));
 }
