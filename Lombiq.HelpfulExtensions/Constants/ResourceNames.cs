c353a50a05028821b2aad5daaa24ea228917a99e
Márk Bartha <barthamark@gmail.com>
2026-03-01 13:56:00 +0100

    Removing leftover code

--- Lombiq.HelpfulExtensions/Constants/ResourceNames.cs
+++ Lombiq.HelpfulExtensions/Constants/ResourceNames.cs
@@ -5,6 +5,4 @@ public static class ResourceNames
     public const string TargetBlank = nameof(TargetBlank);
     public const string Lucide = nameof(Lucide);
     public const string LucideIconPicker = nameof(LucideIconPicker);
-    public const string Prism = nameof(Prism);
-    public const string TrumbowygHighlight = nameof(TrumbowygHighlight);
 }
