aa8a2d1b760d0a5dd35b333ef0f1b112b3b284d2
Sára El-Saig <sara.el-saig@lombiq.com>
2026-06-20 16:49:04 +0200

    Copy fix from OCORE-273.

--- Lombiq.HelpfulExtensions/Extensions/Liquid/Startup.cs
+++ Lombiq.HelpfulExtensions/Extensions/Liquid/Startup.cs
@@ -2,6 +2,7 @@ using Lombiq.HelpfulExtensions.Extensions.Liquid.Services;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Liquid;
 using OrchardCore.Modules;
+using OrchardCore.Workflows.Services;
 
 namespace Lombiq.HelpfulExtensions.Extensions.Liquid;
 
@@ -17,4 +18,11 @@ public sealed class Startup : StartupBase
         services.AddLiquidFilter<ShapesRenderFilter>("shapes_render");
         services.AddLiquidFilter<ShuffleFilter>("shuffle");
     }
+}
+
+[Feature("OrchardCore.Workflows")]
+public sealed class WorkflowStartup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services) => 
+        services.AddScoped<IWorkflowExecutionContextHandler, LiquidViewTemplateWorkflowExecutionContextHandler>();
 }
